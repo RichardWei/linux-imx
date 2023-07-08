1b3be1e4af41db73ad62f421712b8559268af2eb
richard <weizhou.mdgs@gmail.com>
2023-07-08 15:08:54 +0800

    boot  atk rootfs for imx6ull

--- build_imx6_linux.sh
+++ build_imx6_linux.sh
@@ -3,9 +3,9 @@ export PATH=$PATH:/usr/local/arm/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnuea
 # make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx6ull_jerry_emmc_defconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j4
-# mv -f /home/wei/Desktop/linux-imx/arch/arm/boot/zImage /home/wei/linux/nfs/
-# mv -f /home/wei/Desktop/linux-imx/arch/arm/boot/dts/imx6ull-14x14-jerry-emmc.dtb /home/wei/linux/nfs/
-mv -f arch/arm/boot/zImage .
-mv -f arch/arm/boot/dts/imx6ull-14x14-jerry-emmc.dtb .
+mv -f /home/wei/Desktop/linux-imx/arch/arm/boot/zImage /home/wei/linux/nfs/
+mv -f /home/wei/Desktop/linux-imx/arch/arm/boot/dts/imx6ull-14x14-jerry-emmc.dtb /home/wei/linux/nfs/
+# mv -f arch/arm/boot/zImage .
+# mv -f arch/arm/boot/dts/imx6ull-14x14-jerry-emmc.dtb .
 
 
